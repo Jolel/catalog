ff9bcefdd685f83614b401b80221cd44e050da4e
Joel Gonzalez <jolelsoft@gmail.com>
2023-09-06 17:05:58 -0700

    create application repository

---
+++ app/repositories/application_repository.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class ApplicationRepository
+  include Singleton
+
+  delegate :all, to: :@db_table
+  delegate :find, to: :@db_table
+  delegate :includes, to: :@db_table
+  delegate :where, to: :@db_table
+
+  def initialize
+    @db_table = model
+  end
+
+  private
+
+  def model
+    self.class.to_s.gsub('Repository', '').constantize
+  end
+end
